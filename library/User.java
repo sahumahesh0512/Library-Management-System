77296cd7a216d991d6138cde2304d82a54a65624
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2025-01-06 12:33:50 +0530

    Create User.java

---
+++ library/User.java
@@ -0,0 +1,21 @@
+package library;
+
+public abstract class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void displayInfo();
+}
